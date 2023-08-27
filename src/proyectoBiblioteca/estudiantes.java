4ee4c15b77394d44f2e5287177b00212bba5e9be
sarafaustino <141958889+sarafaustino@users.noreply.github.com>
2023-08-26 22:18:14 -0500

    Update estudiantes.java

--- src/proyectoBiblioteca/estudiantes.java
+++ src/proyectoBiblioteca/estudiantes.java
@@ -1,5 +1,45 @@
 package proyectoBiblioteca;
 
+import proyectoBiblioteca.informacionPersona;
+
 public class estudiantes {
+
+// Atributos
+
+    private String programaAcad;
+
+    public estudiantes(String programaAcad) {
+        this.programaAcad = programaAcad;
+    }
+
+    // Métodos
+
+public agregarEstudiante(informacionPersona informacionPersona, String programaAcad) {
+
     
+}
+
+public eliminarEstudiante(estudianteBorrar){
+
+
+}
+
+public modificarEstudiante(){
+
+
+}
+
+public infoEstudiante(){
+    
+
+}
+
+    public String getProgramaAcad() {
+        return programaAcad;
+    }
+
+    public void setProgramaAcad(String programaAcad) {
+        this.programaAcad = programaAcad;
+    }
+
 }
