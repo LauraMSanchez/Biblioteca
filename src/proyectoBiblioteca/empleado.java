e1a88c5c7e4aad9351edf7645a5f9ddb4b58d8d8
JulianAndresNaranjo <142056387+JulianAndresNaranjo@users.noreply.github.com>
2023-08-28 23:16:22 -0500

    Update empleado.java

--- src/proyectoBiblioteca/empleado.java
+++ src/proyectoBiblioteca/empleado.java
@@ -1,59 +1,23 @@
-package proyectoBiblioteca;
+package co.edu.uniquindio.poo;
 
 public class empleado {
-
-    //Atributos
+    //atributos
     private String cargoOcupa;
     private informacionPersona informacionPersona;
     private biblioteca biblioteca;
 
+    //constructor
     public empleado(String cargoOcupa, informacionPersona informacionPersona, biblioteca biblioteca) {
         this.cargoOcupa = cargoOcupa;
         this.informacionPersona = informacionPersona;
         this.biblioteca = biblioteca;
     }
-
-    //Métodos
-    public void agregarEmpleado(String cargoOcupa, informacionPersona informacionPersona, biblioteca biblioteca)
-    {
-
-    }
-    
-    public void borrarEmpleado(informacionPersona informacionPersona, biblioteca biblioteca)
-    {
-
-    }
-
-    public void modificarEmpleado(informacionPersona informacionPersona, biblioteca biblioteca)
-    {
-
-    }
-
-    public void infoEmpleado (informacionPersona informacionPersona, biblioteca biblioteca) 
-    {
-
-    }
-
-
-    //Getter Setter
-        public String getCargoOcupa() {
+    //Setter y Getter
+    public String getCargoOcupa() {
         return cargoOcupa;
     }
 
-     public informacionPersona getInformacionPersona() {
-        return informacionPersona;
-    }
-
-    public void setInformacionPersona(informacionPersona informacionPersona) {
-        this.informacionPersona = informacionPersona;
-    }
-
-    
-    public biblioteca getBiblioteca() {
-        return biblioteca;
-    }
-
-    public void setBiblioteca(biblioteca biblioteca) {
-        this.biblioteca = biblioteca;
+    public void setCargoOcupa(String cargoOcupa) {
+        this.cargoOcupa = cargoOcupa;
     }
 }
